9e15b9ad8d74da9f84f2a2d8c61e864ee855b194
JuanPa0109 <jusanchezre@unal.edu.co>
2024-12-09 23:19:56 -0500

    Add files via upload

---
+++ Ejercicio_propuesto_18.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_propuesto_18;
+
+/**
+ *
+ * @author Acer
+ */
+
+import java.util.Scanner;
+
+
+public class Ejercicio_propuesto_18 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Ingrese el código del empleado: ");
+            int codigoEmpleado = sc.nextInt();
+            sc.nextLine();
+            
+            System.out.print("Ingrese los nombres del empleado: ");
+            String nombres = sc.nextLine();
+            
+            
+            System.out.print("Ingrese el número de horas trabajadas al mes: ");
+            int horasTrabajadas = sc.nextInt();
+            
+            System.out.print("Ingrese el valor por hora trabajada: ");
+            double valorHora = sc.nextDouble();
+            
+            System.out.print("Ingrese el porcentaje de retención en la fuente: ");
+            double retencion = sc.nextDouble();
+            
+            double salarioBruto = horasTrabajadas * valorHora;
+            double salarioNeto = salarioBruto - (salarioBruto * (retencion / 100));
+            
+            System.out.println("\nCódigo del empleado: " + codigoEmpleado);
+            System.out.println("Nombres del empleado: " + nombres);
+            System.out.println("Salario bruto: $" + salarioBruto);
+            System.out.println("Salario neto: $" + salarioNeto);
+        }
+    }
+}
+
